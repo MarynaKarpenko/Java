b1b09ac03e7d350ebe3d63504dca444bfb7a623e
marina <mariina.karpenko@gmail.com>
2023-06-27 22:36:46 +0200

    new

--- Home-work4/src/main/java/de/telran/Task1.java
+++ Home-work4/src/main/java/de/telran/Task1.java
@@ -1,2 +1,35 @@
-package de.telran;public class Task1 {
+package de.telran;
+
+public class Task1 {
+    public static void main(String[] args) {
+        // 1. Создайте строку через new - I study Basic Java!
+        String myStr = new String("I study Basic Java!");
+
+        // 2. Напишите метод, который принимает в качестве параметра строку,
+        // передайте в этот метод строку, которую создали в п.1
+        System.out.println(myStr);
+
+        // 3. Распечатать пред-последний символ строки. Используем метод String.charAt().
+        System.out.println(myStr.charAt(myStr.length()-2));
+
+        // 4. Проверить, содержит ли ваша строка подстроку “Java”.
+        // Используем метод String.contains().
+        boolean containsJava = myStr.contains("Java");
+        System.out.println("Строка содержит подстроку \"Java\": "+containsJava);
+
+        // 5. Вырезать строку Java c помощью метода String.substring().
+        System.out.println(myStr.substring(14, 18));
+
+        // 6. Заменить все символы “а” на “о”.
+        String replacedStr = myStr.replace('a', 'o');
+        System.out.println("Новая строка: "+replacedStr);
+
+        // 7. Преобразуйте строку к верхнему регистру.
+        String upperCaseStr = myStr.toUpperCase();
+        System.out.println("Строка в верхнем регистре: "+upperCaseStr);
+
+        // 8. Преобразуйте строку к нижнему регистру.
+        String lowerCaseStr = myStr.toLowerCase();
+        System.out.println("Строка в нижнем регистре: "+lowerCaseStr);
+    }
 }
